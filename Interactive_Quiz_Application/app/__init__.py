0f1c9f707a71da4f194146f4f29dd074de484678
ISSAM <issam.slimani.maroc@gmail.com>
2024-12-18 20:33:10 +0100

    set up Database Design (Storing User Data & Results)

--- Interactive_Quiz_Application/app/__init__.py
+++ Interactive_Quiz_Application/app/__init__.py
@@ -1,23 +1,34 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from app.models import users_db
+#from app.models import users_db
  
 
 app = Flask(__name__)
 app.secret_key = 'd28ebc859848fe4f6b9154bffefdc230'
+# Database setup
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quizapp.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the database
+db = SQLAlchemy(app)
 
 # Initialize Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "auth.login"
 
+# User Loader
+from app.models import User
+
 @login_manager.user_loader
 def load_user(user_id):
-    print(f"Attempting to load user with ID: {user_id}")
-    user = users_db.get(user_id)
-    print(f"Loaded user: {user}")
-    return user
+    #user = users_db.get(user_id)
+    return User.query.get(int(user_id))
 
-from . import routes, auth
+#from . import routes, auth
 # Import routes for authentication and regular app functionality
-app.register_blueprint(auth.bp)
+#app.register_blueprint(auth.bp)
+# Create all the tables in the database
+with app.app_context():
+    db.create_all()
